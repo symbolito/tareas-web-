286991a4c44f69758e46e5eaa938aa42a3be37cc
symbolito <blancosebas34@gmail.com>
2025-08-25 20:07:20 -0500

    Update back.js

--- back.js
+++ back.js
@@ -55,8 +55,13 @@ app.delete("/tareas/:id", (req, res) => {
 
 
 const PORT = 8000;
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, '0.0.0.0', () => 
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
 );
+
 
 
